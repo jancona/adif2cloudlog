5bdddd2a8e65a2d13bde69528f1eb7b787e3d60c
Jim Ancona <jim@anconafamily.com>
2019-09-07 19:46:50 -0400

    Get API key from environment, other clean-up

--- main.go
+++ main.go
@@ -6,23 +6,34 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/hpcloud/tail"
 )
 
-const apiKey = "cl5d7071f3082be"
+var apiKey string
 
 func main() {
+	var found bool
+	apiKey, found = os.LookupEnv("CLOUDLOG_API_KEY")
+	if !found {
+		log.Fatal("CLOUDLOG_API_KEY must be set")
+	}
 	if len(os.Args) != 3 {
-		log.Fatal("Usage adif2logcloud <adiflog> <logcloudurl>")
+		log.Fatal("Usage adif2logcloud <ADIF log> <cloudlog url>\n  Example adif2logcloud ~/.local/share/WSJT-X/wsjtx_log.adi http://localhost/cloudlog")
 	}
 	file := os.Args[1]
 	t, err := tail.TailFile(file, tail.Config{Follow: true})
 	if err != nil {
 		log.Fatalf("Error tailing %s: %v", file, err)
 	}
+	url := os.Args[2]
+	if !strings.HasSuffix(url, "/") {
+		url += "/"
+	}
+	url += "index.php/api/qso"
 	for line := range t.Lines {
-		sendLine(line.Text)
+		sendLine(line.Text, url)
 	}
 }
 
@@ -33,12 +44,12 @@ type cloudlogRequest struct {
 	Status string `json:"status,omitempty"`
 }
 
-func sendLine(line string) {
+func sendLine(line string, url string) {
 	log.Printf("Sending '%s", line)
 	req := cloudlogRequest{APIKey: apiKey, Type: "adif", Line: line}
 	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(req)
-	resp, err := http.Post(os.Args[2], "application/json", buf)
+	resp, err := http.Post(url, "application/json", buf)
 	if err != nil {
 		log.Fatalf("Error posting to %s: %v", os.Args[2], err)
 	}
